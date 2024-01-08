6cdadbcf3c88a572cf7b103b1a70e113e8b8b524
vibhav <vibhav.agarwal@publicissapient.com>
2024-01-07 18:40:57 -0600

    Implementation of factory design pattern

---
+++ Factory Design Pattern/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        ShapeFactory factory= new ShapeFactory();
+        Shape shape= factory.getShape("circle");
+        shape.draw();
+    }
+}
---
+++ Factory Design Pattern/src/ShapeFactory.java
@@ -0,0 +1,16 @@
+public class ShapeFactory {
+
+
+
+    public Shape getShape(String input)
+    {
+        switch (input){
+            case "circle":
+                return new Circle();
+            case "rectangle":
+                return new Rectangle();
+            default:
+                return null;
+        }
+    }
+}
